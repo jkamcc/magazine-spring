9d38f7b8f40fc4a43dfc2d74bb73775372a15952
sharon1691 <sharoncm.1691@gmail.com>
2013-10-15 16:13:08 -0700

    Cambio en interfaz del error

--- src/main/java/com/app/web/HomeController.java
+++ src/main/java/com/app/web/HomeController.java
@@ -10,12 +10,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController extends AbstractController  {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -64,6 +69,12 @@ public class HomeController {
 	@RequestMapping(value="/error", method = RequestMethod.GET)
 	public String error(Model model) {
 		return "error";
+	}
+	
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+		HttpServletResponse response) throws Exception {
+		throw new GenericException("E888", "This is custom message - ABC");
 	}
 	
 }
--- src/main/webapp/WEB-INF/views/error.jsp
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -1,8 +1,12 @@
 <%@include file="init.jsp" %>
 
-<div style="text-align: center">
-	<h3> Error </h3>
-	<h5> Disculpe, se ha producido un error en la aplicacion </h5>
-	<img src='<c:url value="/resources/img/error_image.jpg"/>' alt="Imagen de error" width="300" height="310">
+<div class="errorDiv" style="display: inline-block;">
+	<div class="imagenError" style="float: left;">
+		<img src='<c:url value="/resources/img/error_image.jpg"/>' alt="Imagen de error" width="300" height="310">
+	</div>
 	
+	<div class ="textoError">
+		<h3> Error </h3>
+		<h5> Disculpe, se ha producido un error en la aplicaci&oacuten </br> Por favor ingrese a otra de las opciones del men&uacute  </h5>
+	</div>
 </div>
