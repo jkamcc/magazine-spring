f50355517434f64f44a478eb04d8d505a8e7adcd
karenmh09 <karenmh09@gmail.com>
2013-10-19 19:06:52 -0600

    Nuevo Edit Article

--- rename.bat
+++
@@ -1,9 +0,0 @@
-
-set Pathcopy="C:\tomcat7.0.42\webapps"
-set Pathname="C:\Users\Karen\git\magazine-spring\target"
-cd %Pathname%
-
-copy web-1.0.0-BUILD-SNAPSHOT.war %Pathcopy%\magazine.war
-
-set Pathname="C:\Users\Karen\git"
-cd %Pathname%
