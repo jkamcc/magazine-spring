6f6e0ebf59c451ed9864b0cfcdabe5ce6e2d8735
sharon1691 <sharoncm.1691@gmail.com>
2013-11-07 07:14:15 -0800

    SectionService

--- service/src/main/java/org/jks/web/SectionController.java
+++ service/src/main/java/org/jks/web/SectionController.java
@@ -1,7 +1,23 @@
 package org.jks.web;
 
 /**
- * @author juancarrillo
+ * @author scorrales
  */
+
+@Controller
+@RequestMapping("section")
 public class SectionController {
+	
+	 private static final Logger logger = LoggerFactory.getLogger(SectionController.class);
+
+	    @Inject
+	    private SectionService sectionService;
+	    
+	    @RequestMapping(value="addSection", method=RequestMethod.POST)
+	    @ResponseBody
+	    public String saveSection(Section section) {
+	        sectionService.addSection(section);
+	        return "Seccion agregada: " + section.toString();
+	    }
+
 }
