2df201a82c1f1295c846b61667f5bf84c92300ee
sharon1691 <sharoncm.1691@gmail.com>
2013-11-14 16:32:45 -0800

    Cliente ya inserta seccion llamando al web service

--- client/src/main/java/org/jks/web/SectionController.java
+++ client/src/main/java/org/jks/web/SectionController.java
@@ -14,10 +14,14 @@ import org.jks.domain.Article;
 import org.jks.domain.Section;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -35,5 +39,18 @@ public class SectionController {
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("sectionList",  slist);
 		return new ModelAndView("showSections", model);
+	}
+	
+	/* Obtiene la lista de todas las secciones en la BD */
+	@RequestMapping(value="/create", method = RequestMethod.POST)
+	public ModelAndView addSection(@RequestParam String nuevaSeccion) {
+		/* Creacion del objeto a enviar*/
+		Section section= new Section();
+		section.setSectionArticle(nuevaSeccion);
+		List<HttpMessageConverter<?>> list = new ArrayList<HttpMessageConverter<?>>();
+		list.add(new MappingJacksonHttpMessageConverter());
+		restTemplate.setMessageConverters(list);
+		restTemplate.postForObject("http://localhost:8080/service/section/create", section, Section.class);
+		return new ModelAndView("addSection");
 	}
 }
