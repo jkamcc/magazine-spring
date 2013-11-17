ed6b6244169b29b453da3e0d14e26cdf3b6d37cf
karenmh09 <karenmh09@gmail.com>
2013-11-16 19:20:38 -0600

    Inicio de la validacion para creacion del articulo

--- client/src/main/java/org/jks/web/ArticleController.java
+++ client/src/main/java/org/jks/web/ArticleController.java
@@ -1,22 +1,22 @@
 package org.jks.web;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.jks.domain.Article;
 import org.jks.domain.Section;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-
-import antlr.collections.List;
 
 
 @Controller
@@ -26,25 +26,31 @@ public class ArticleController {
 	@Autowired
     private RestTemplate restTemplate;
 	
-	/* 
+	/*
 	 * La este es el que con el form va a jalar los datos y crear el articulo para mandarlo a bd
-	@RequestMapping(value="/savearticle", method = RequestMethod.GET)
-	public String savearticle(Model model) {
-		//Article article = restTemplate.getForObject("http://localhost:8080/service/user/1", Article.class);
-		//System.out.println("article.getSubject() = " + article.getSubject());
-		return "";
-	}
 	*/
+	@RequestMapping(value="/savearticle", method = RequestMethod.POST)
+	public String savearticle(@Valid Article article, BindingResult result, Model model) {
+		try {
+            restTemplate.postForObject("http://localhost:8080/service/user/create", article, Article.class);
+
+        } catch (RestClientException e) {
+            e.printStackTrace();
+            model.addAttribute("error", e.getStackTrace());
+        }
+
+        return "articlecreated";
+	}
+	
 	
 	//Este metodo lleva al usuario a editarticle.jsp para que pueda crear un articulo
     @RequestMapping(value="/newarticle", method = RequestMethod.GET)
-    public ModelAndView newarticle(Model m) {
+    public String newarticle(Model model) {
     	Section[] sections = restTemplate.getForObject("http://localhost:8080/service/section/showSections", Section[].class);
 
-    	Map<String, Object> model = new HashMap<String, Object>();
-		model.put("sectionList",  sections);
+		model.addAttribute("sectionList",  sections);
 		
-    	return new ModelAndView("editarticle", model);
+    	return "editarticle";
     }
     
     
--- client/src/main/webapp/WEB-INF/views/editarticle.jsp
+++ client/src/main/webapp/WEB-INF/views/editarticle.jsp
@@ -5,13 +5,13 @@
 <div id="editarticlediv">
 
 <form id="editarticle_fm" name="editarticle_fm" action="<c:url value='/articles/savearticle' />" method='POST'>
-		<p>
-			<input name="article_name" type="text" class="input-block-level" placeholder="Título">
-		</p>
-		<p>
+		<div>
+			<input name="subject" type="text" class="input-block-level" placeholder="Título">
+		</div>
+		<div>
 			<c:if test="${!empty sectionList}">
-				Indique la sección del artículo: <br>
-				<select>
+				Indique la sección del artículo:
+				<select name="sectionid">
 					<c:forEach items="${sectionList}" var="section">
 						<option value="<c:out value="${section.sectionid}"/>">
 							<c:out value="${section.sectionArticle}"/>
@@ -19,11 +19,32 @@
 					</c:forEach>
 				</select> 
 			</c:if>
-		</p>
-		<p>
-			<textarea rows="15" cols="50" name="article_content" class="input-block-level" placeholder="Contendio del artículo">
+		</div>
+		<div>
+			<input name="author" type="text" class="input-block-level" placeholder="Autor del artículo" >
+		</div>
+		<div>
+			<textarea rows="7" cols="50" name="article" class="input-block-level" placeholder="Contendio del artículo">
 			</textarea>
-		</p>
+		</div>
 		<button id="btn-editarticle" type="submit">Crear Artículo</button>
 	</form>
 </div>
+
+<script type="text/javascript" src='<c:url value="/resources/js/jquery.validate.js"/>'>
+    $(document).ready(function(){
+        $('#editarticle_fm').validate({
+            rules: {
+                article: {
+                    required: true
+                },
+                subject: {
+                    required: true
+                },
+                author: {
+                    required : true
+                }
+            }
+        });
+    });
+</script>
